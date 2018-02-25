085d0c217f53ce370402f3d959ec84a65a891890
mariti8 <maritimonina8@gmail.com>
2018-02-25 21:05:25 +0300

    vse_otchet

--- Palindrome.java
+++ Palindrome.java
@@ -5,6 +5,10 @@ public class Palindrome
 {
 	public static String reverseString(String s)
 	{
+		//создаем пустую строку 
+		//переворачиваем слово поэлементно записывая с конца 
+		//в начало в строк s1
+		//возвращаем строку
 		String s1 = "";
 		for (int i = s.length()-1; i >= 0; i--) { 
 			s1=s1+s.charAt(i);
@@ -15,14 +19,16 @@ public class Palindrome
 
 	public static boolean isPalindrome(String s)
 	{
+		//проверяем является ли строка полиномом
+		//возвращаем ди или нет(правда или ложь)
 		String s1=reverseString(s);
 		return s1.equals(s);
 	}
-	/**
-	* функция main
-	*/
+	
 	public static void main(String[] args)
 	{
+		//основная функция, собирающая воедино все функции
+		// и реализующая их работу
 	 	for (int i = 0; i < args.length; i++) 
  		{
  			System.out.println(isPalindrome(args[i]));
